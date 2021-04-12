51ef0004a4934943f19a7eafb2cf3d0832007fdb
Rodje Ross <rodjeross10@yahoo.com>
2021-04-11 19:44:12 -0500

    Exercise 1 completed

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
 
+from flask_uploads import UploadSet, IMAGES
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired, TextAreaField
+
+images = UploadSet('images', IMAGES)
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description [500 words max]', validators=[DataRequired()])
+    upload = FileField('Photo', validators=[FileRequired(), FileAllowed(images, 'Images only!')])
